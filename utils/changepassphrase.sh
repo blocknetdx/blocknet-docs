7d33c78c7d85e309f1d4f6b841cedf634b688aaf
plortp <31665760+plortp@users.noreply.github.com>
2017-09-05 18:11:02 +0100

    Create changepassphrase.sh

---
+++ utils/changepassphrase.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Don't unlock via cli as your password is stored in the history
+set -e
+
+read -s -p 'blocknetdx wallet current passphrase: ' passphrase_current
+read -s -p "$(echo -e '\nblocknetdx wallet new passphrase: ')" passphrase_new
+read  -s -p "$(echo -e '\nblocknetdx wallet confirm new passphrase: ')"  passphrase_confirm
+
+if [ "$passphrase_new" == "$passphrase_confirm" ]; then
+
+echo -e "\n"
+
+./blocknetdx-cli walletpassphrasechange ${passphrase_current} ${passphrase_new}
+
+else
+
+echo -e "\npasswords do not match"
+
+fi
+
+exit 0
